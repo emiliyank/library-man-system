bb242f8810450c498f213c7384150955dd510dfc
DKichukov <dzhamal.kichukov@gmail.com>
2023-01-30 18:08:12 +0200

    Initial commit

---
+++ src/main/java/library/LibraryManSystemApplication.java
@@ -0,0 +1,13 @@
+package library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManSystemApplication.class, args);
+    }
+
+}
